e48227b6473bc6d603c3e2209f5e201c29c04894
pranaytummalapalli <pranay.tummalapalli@gmail.com>
2024-09-12 19:50:29 +0530

    editted some more stuff

--- shmem/mem_basics/m_alloc.c
+++ shmem/mem_basics/m_alloc.c
@@ -7,10 +7,13 @@ static int increment_size = 1;
 static int block_size = sizeof(int); //init block size the user wants to allocate. Taken int for now, convert later to user input 
 
 void *m_alloc(size_t size); //prototype of custom malloc fn
+void *m_free(void* mem);
 
 int main(int argc, char* argv[]){
     int *a = malloc(sizeof(int));
     int *newMemory = m_alloc(block_size);
+    m_free(newMemory);
+    a = newMemory;
 
     printf("Malloc assigned: %p\nCustom assigned: %p", a, newMemory);
     
@@ -21,4 +24,11 @@ void *m_alloc(size_t size){
     void* newBrk = sbrk(size);
     void* newMem = prevBrk;
 
+}
+
+void *m_free(void* mem){
+    void* setbackMem = mem - block_size;
+    if(brk(setbackMem) < 0){
+        perror("Could not reset heap-end");
+    };
 }

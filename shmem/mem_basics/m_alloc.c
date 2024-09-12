fb1223c2ff1681fbc108eaa08d6e819aedff4b1c
pranaytummalapalli <pranay.tummalapalli@gmail.com>
2024-09-12 17:49:18 +0530

    basic heap-end-pushing memory assignment

--- shmem/mem_basics/m_alloc.c
+++ shmem/mem_basics/m_alloc.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
 
-int main(int argc, char** argv){
+static int increment_size = 1;
+static int block_size = sizeof(int); //init block size the user wants to allocate. Taken int for now, convert later to user input 
+
+void *m_alloc(size_t size); //prototype of custom malloc fn
+
+int main(int argc, char* argv[]){
     int *a = malloc(sizeof(int));
-    printf("%p", a);    
+    int *newMemory = m_alloc(block_size);
+
+    printf("Malloc assigned: %p\nCustom assigned: %p", a, newMemory);
+    
+}
+
+void *m_alloc(size_t size){
+    void* prevBrk = sbrk(size);
+    void* newBrk = sbrk(size);
+    void* newMem = prevBrk;
+
 }
